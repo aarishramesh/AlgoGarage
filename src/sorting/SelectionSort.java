35b1065efa88a4d470e4bb07630d6fa9fa82f1e9
aarish <aarish.ramesh@gmail.com>
2014-12-10 13:01:14 +0530

    Sorting abstraction

---
+++ src/sorting/SelectionSort.java
@@ -0,0 +1,16 @@
+package sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectionSort {    
+    private static final SelectionSort INSTANCE = new SelectionSort();
+    
+    public SelectionSort getInstance() {
+        return INSTANCE;
+    }    
+    
+    public void sort() {
+        
+    }
+}
