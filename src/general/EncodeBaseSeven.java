ec62e975cc6d33c937a5deedb26264fb52e303d7
aarishramesh <aarish.ramesh@gmail.com>
2017-08-26 19:03:12 +0530

    ATlassian problems solved

---
+++ src/general/EncodeBaseSeven.java
@@ -0,0 +1,60 @@
+package general;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class EncodeBaseSeven {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println(convertToBase2(num));
+	}
+
+	private static String convertToBase7(int num) {
+		Stack<Character> resultStack = new Stack<Character>();
+		while (num > 0) {
+			int rem = num % 7;
+			Character charVal = getEncodedChar(rem);
+			resultStack.push(charVal);
+			num = num / 7;
+		}
+		StringBuilder result = new StringBuilder();
+		while(!resultStack.isEmpty())
+			result.append(resultStack.pop());
+		return result.toString();
+	}
+
+	private static String convertToBase2(int num) {
+		Stack<Character> resultStack = new Stack<Character>();
+		while (num > 0) {
+			int rem = num % 2;
+			Character charVal = getEncodedChar(rem);
+			resultStack.push(charVal);
+			num = num / 2;
+		}
+		StringBuilder result = new StringBuilder();
+		while(!resultStack.isEmpty())
+			result.append(resultStack.pop());
+		return result.toString();
+	}
+	
+	private static Character getEncodedChar(int num) {
+		//0 a t l s i N
+		switch(num) { 
+			case 1:
+				return 'a';
+			case 2:
+				return 't';
+			case 3:
+				return 'l';
+			case 4:
+				return 's';
+			case 5:
+				return 'i';
+			case 6:
+				return 'N';
+			default:
+				return '0';	
+		}
+	}
+}
---
+++ src/general/RobotPML.java
@@ -0,0 +1,77 @@
+package general;
+
+public class RobotPML {
+	public static void main(String args[]) throws Exception {
+		System.out.println(compute("PMMMMMMMMMMMMMMMMMMMLPLPLPLPLPLPLPLPLPLPL"));
+	}
+
+	static String compute(String instructions) {
+		StringBuilder sb = new StringBuilder();
+		char[] chars = instructions.toCharArray();
+		int[] intOut = new int[10];
+		int count = 0;
+		int position = 0;
+		for (int i = 0; i < chars.length; i++) {
+
+			if (chars[i] == 'P') {
+				count = 1;
+			} else if (chars[i] == 'M') {
+				if (position < 9) {
+					position++;
+				}
+			} else if (chars[i] == 'L') {
+				intOut[position] = intOut[position] + count;
+				position = 0;
+				count = 0;
+			}
+
+		}
+
+		for (int i = 0; i < intOut.length; i++) {
+			sb.append(getChar(intOut[i]));
+		}
+		return sb.toString();
+
+	}
+
+	private static char getChar(int i) {
+		switch (i) {
+		case 0:
+			return '0';
+		case 1:
+			return '1';
+		case 2:
+			return '2';
+		case 3:
+			return '3';
+		case 4:
+			return '4';
+		case 5:
+			return '5';
+		case 6:
+			return '6';
+		case 7:
+			return '7';
+		case 8:
+			return '8';
+		case 9:
+			return '9';
+		case 10:
+			return 'A';
+		case 11:
+			return 'B';
+		case 12:
+			return 'C';
+		case 13:
+			return 'D';
+		case 14:
+			return 'E';
+		default:
+			if (i > 14) {
+				return 'E';
+			}
+		}
+		return '-';
+	}
+
+}
