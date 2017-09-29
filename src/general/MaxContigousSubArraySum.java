9488bae41fd0d6c4299d88b596f42bc5b62e8062
aarishramesh <aarish.ramesh@gmail.com>
2017-09-29 12:55:40 +0530

    refactor

--- src/general/MaxContigousSubArraySum.java
+++ src/general/MaxContigousSubArraySum.java
@@ -1,5 +1,7 @@
 package general;
 
+import java.util.Scanner;
+
 /**
  * Kadane's Algorithm
  * 
@@ -13,12 +15,18 @@ package general;
  */
 public class MaxContigousSubArraySum {
 	public static void main(String[] args) {
-		int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
 		int max_so_far = 0, max_ending_here = 0;
 		for (int i = 0; i < arr.length; i++) {
 			max_ending_here = Math.max(max_ending_here + arr[i], arr[i]);
 			max_so_far = Math.max(max_ending_here, max_so_far);
 		}
 		System.out.println(max_so_far);
+		sc.close();
 	}
 }
