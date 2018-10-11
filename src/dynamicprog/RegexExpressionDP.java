d3410596d8df5d50f0a4b66e7849a0acd3ac6913
Aarish Ramesh <aarish.ramesh@gmail.com>
2018-10-12 00:58:55 +0530

    Regex matching problem solution

---
+++ src/dynamicprog/RegexExpressionDP.java
@@ -0,0 +1,59 @@
+package dynamicprog;
+
+/**
+ * Write a program to perform regex matching with * an . 
+ * 
+ * References : http://leetcode.com/2011/09/regular-expression-matching.html
+ */
+public class RegexExpressionDP {
+	public static void main(String[] args) {
+		System.out.println(regexMatchingDp("xaabyc", ".*"));
+	}
+
+	public static boolean regexMatchingDp(String text, String regex) {
+		boolean[][] regexMatch = new boolean[text.length() + 1][regex.length() + 1];
+		
+		regexMatch[0][0] = true;
+		
+		for (int i = 1; i < text.length() + 1; i++)
+			regexMatch[i][0] = false;
+		
+		// Does regex match empty string ?
+		boolean emptyRegexMatch = true;
+		if (regex.equals(".*")) {
+			int j = 1;
+			while (j < regex.length()) {
+				if (regex.charAt(j) == '*') {
+					j += 2;
+					continue;
+				}
+				emptyRegexMatch = false;
+				break;
+			}
+		}
+		if (emptyRegexMatch) {
+			for (int j = 1; j < regex.length() + 1; j++) {
+				regexMatch[0][j] = emptyRegexMatch;
+			}
+		}
+		
+		for (int i = 1; i < text.length() + 1; i++) {
+			for (int j = 1; j < regex.length() + 1; j++) {
+				if (text.charAt(i - 1) == regex.charAt(j - 1) || regex.charAt(j - 1) == '.') {
+					regexMatch[i][j] = regexMatch[i - 1][j - 1];
+				} else {
+					if (regex.charAt(j - 1) == '*') {
+						regexMatch[i][j]	 = regexMatch[i][j - 2];
+						if (text.charAt(i - 1) == regex.charAt(j - 2) ||
+								regex.charAt(j - 2) == '.') {
+							regexMatch[i][j] = regexMatch[i][j] | regexMatch[i - 1][j];
+						}
+					} else {
+						regexMatch[i][j] = false;
+					}
+				}
+			}
+		}
+		return regexMatch[text.length()][regex.length()];
+	}
+}
