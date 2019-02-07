5cb9e5430fa3f2a94f3a89998656b53fbce64f9c
Aarish Ramesh <aarish.ramesh@gmail.com>
2019-02-07 20:17:22 +0530

    Wave array linear time impl

--- src/arraysstrings/WaveArray.java
+++ src/arraysstrings/WaveArray.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 
 /**
  * https://www.interviewbit.com/problems/wave-array/
+ * https://www.geeksforgeeks.org/sort-array-wave-form-2/
  * 
  * @author polymath
  *
@@ -16,6 +17,7 @@ public class WaveArray {
 		arr.add(6); arr.add(1); arr.add(2); arr.add(3); arr.add(4); arr.add(5); arr.add(6);
 		
 		System.out.println(wave(arr));
+		System.out.println(waveTwo(arr));
 	}
 	
 	public static ArrayList<Integer> wave(ArrayList<Integer> A) {
@@ -34,4 +36,26 @@ public class WaveArray {
 		}
 		return null;
     }
+	
+	public static ArrayList<Integer> waveTwo(ArrayList<Integer> A) {
+		if (A != null && !A.isEmpty()) {
+			if (A.size() == 1 || A.size() == 2)
+				return A;
+			for (int i = 0; i < A.size(); i++) {
+				if (i - 1 >= 0 && A.get(i - 1) > A.get(i))
+					swap(A, i - 1, i);
+				if (i + 1 < A.size() && A.get(i + 1) > A.get(i))
+					swap(A, i, i + 1);
+			}
+		}
+		return A;
+	}
+	
+	private static void swap(ArrayList<Integer> A, int indexA, int indexB) {
+		if (A != null && indexA > indexB && indexB < A.size()) {
+			int temp = A.get(indexA);
+			A.set(indexA, A.get(indexB));
+			A.set(indexB, temp);
+		}
+	}
 }
