01d2685170351be13e83316d7c2391a51763fbe9
Aarish Ramesh <aarish.ramesh@gmail.com>
2019-03-05 23:01:49 +0530

    A variation of implementation added

--- src/arraysstrings/CountUniquePairs.java
+++ src/arraysstrings/CountUniquePairs.java
@@ -16,11 +16,12 @@ import java.util.Map;
  */
 public class CountUniquePairs {
 	public static void main(String[] args) {
-		int[] arr = {1, 2, 2, 3, 4, 5};
+		int[] arr = {3, 4, 2, 6};
 		System.out.println(countDiffPairsUsingHashing(arr, 1));
 		System.out.println(countDiffPairsUsingSorting(arr, 1));
+		System.out.println(countDiffPairsUsingHashing2(arr, 1));
 	}
-	
+
 	private static int countDiffPairsUsingHashing(int[] arr, int diff) {
 		Map<Integer, Integer> freqMap = new HashMap<Integer, Integer>();
 		for (int i = 0; i < arr.length; i++)
@@ -41,9 +42,30 @@ public class CountUniquePairs {
 		return count;
 	}
 
+	public static boolean countDiffPairsUsingHashing2(int[] nums, int target) {
+
+		if (nums != null && nums.length > 0) {
+			HashMap<Integer, Integer> numVsFreq = new HashMap<Integer, Integer>();
+			for (int i = 0; i < nums.length; i++) {
+				numVsFreq.put(nums[i], numVsFreq.getOrDefault(nums[i], 0) + 1);   
+			}
+
+			for (int i = 0; i < nums.length; i++) {
+				int diff = target - nums[i];
+				numVsFreq.put(nums[i], numVsFreq.get(nums[i]) - 1);
+
+				if (numVsFreq.get(diff) != null && numVsFreq.get(diff) > 0) {
+					return true;
+				}
+				numVsFreq.put(nums[i], numVsFreq.get(nums[i] + 1));
+			}
+		}
+		return false;
+	}
+	
 	private static int countDiffPairsUsingSorting(int[] arr, int diff) {
 		Arrays.sort(arr);
-		int l = 0, r = 0, count = 0;
+		int l = 0, r = 1, count = 0;
 		while (r < arr.length) {
 			if (arr[r] - arr[l] == diff) {
 				l++; r++;
