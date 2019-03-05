f94c04e2c269a6f484161aaa5ea1770d6e9ea57a
Aarish Ramesh <aarish.ramesh@gmail.com>
2019-03-05 22:59:19 +0530

    Input validations added

--- src/stack/MaxAreaOfHistogram.java
+++ src/stack/MaxAreaOfHistogram.java
@@ -4,42 +4,45 @@ import java.util.Stack;
 
 public class MaxAreaOfHistogram {
 	public static void main(String[] args) {
-		int[] arr = {2,2,2,6,1,5,4,2,2,2,2};
+		int[] arr = {1, 3, 4, 2};
 		int maxArea = findMaxAreaHistogram(arr);
 		System.out.println(maxArea);
 		assert maxArea == 12;
 	}
-	
+
 	private static int findMaxAreaHistogram(int[] arr) {
-		Stack<Integer> stack = new Stack<Integer>();
-		int topIdx, area, maxArea = 0, i = 0;
-		for (i = 0; i < arr.length; i++) {
-			if (stack.isEmpty() || arr[stack.peek()] <= arr[i]) {
-				stack.push(i);
-			} else {
-				while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
-					topIdx = stack.pop();
-					if (stack.isEmpty()) {
-						area = arr[topIdx] * i;
-					} else {
-						area = arr[topIdx] * (i - stack.peek() - 1);
+		int maxArea = 0;
+		if (arr != null && arr.length > 0) {
+			Stack<Integer> stack = new Stack<Integer>();
+			int topIdx, area, i = 0;
+			for (i = 0; i < arr.length; i++) {
+				if (stack.isEmpty() || arr[stack.peek()] <= arr[i]) {
+					stack.push(i);
+				} else {
+					while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+						topIdx = stack.pop();
+						if (stack.isEmpty()) {
+							area = arr[topIdx] * i;
+						} else {
+							area = arr[topIdx] * (i - stack.peek() - 1);
+						}
+						if (area > maxArea)
+							maxArea = area;
 					}
-					if (area > maxArea)
-						maxArea = area;
+					stack.push(i);
 				}
-				stack.push(i);
 			}
-		}
-		
-		while (!stack.isEmpty()) {
-			topIdx = stack.pop();
-			if (stack.isEmpty()) {
-				area = arr[topIdx] * i;
-			} else {
-				area = arr[topIdx] * (i - stack.peek() - 1);
+
+			while (!stack.isEmpty()) {
+				topIdx = stack.pop();
+				if (stack.isEmpty()) {
+					area = arr[topIdx] * i;
+				} else {
+					area = arr[topIdx] * (i - stack.peek() - 1);
+				}
+				if (area > maxArea)
+					maxArea = area;
 			}
-			if (area > maxArea)
-				maxArea = area;
 		}
 		return maxArea;
 	}
