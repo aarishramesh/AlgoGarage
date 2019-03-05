02a9e837fd3a2ad3ed8a468395b904474dd98ad5
Aarish Ramesh <aarish.ramesh@gmail.com>
2019-03-05 22:46:24 +0530

    Optimisation by replacing String with StringBuilder

--- src/backtracking/GenerateAllParenthesis.java
+++ src/backtracking/GenerateAllParenthesis.java
@@ -7,26 +7,29 @@ public class GenerateAllParenthesis {
 	static ArrayList<String> parenthesis = new ArrayList<String>();
 
 	public static void main(String[] args) {
-		System.out.println(generateParenthesis(3).toString());
+		System.out.println(generateParenthesis(4).toString());
 	}
 
 	public static ArrayList<String> generateParenthesis(int A) {
-		generateParenthesisInternal(A - 1, A	, "(");
+		generateParenthesisInternal(A - 1, A	, new StringBuilder("("));
 		return parenthesis;
 	}
 
-	static void generateParenthesisInternal(int openBraces, int closedBraces, String parenthesisStr) {
+	static void generateParenthesisInternal(int openBraces, int closedBraces, StringBuilder parenthesisStr) {
 		
 		if (openBraces < 0 || closedBraces < 0)
 			return;
 		
 		if (openBraces == 0 && closedBraces == 0 && parenthesisStr.charAt(parenthesisStr.length() - 1) == ')') {
-			parenthesis.add(parenthesisStr);
+			parenthesis.add(parenthesisStr.toString());
 			return;
 		} 
 
-		generateParenthesisInternal(openBraces - 1, closedBraces, parenthesisStr + "(");
-		if (openBraces < closedBraces)
-			generateParenthesisInternal(openBraces, closedBraces - 1, parenthesisStr + ")");
+		generateParenthesisInternal(openBraces - 1, closedBraces, parenthesisStr.append("("));
+		parenthesisStr.deleteCharAt(parenthesisStr.length() - 1);
+		if (openBraces < closedBraces) {
+			generateParenthesisInternal(openBraces, closedBraces - 1, parenthesisStr.append(")"));
+			parenthesisStr.deleteCharAt(parenthesisStr.length() - 1);
+		}
 	}
 }
