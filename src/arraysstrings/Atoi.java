150312e57aacd46b9fce77d88f5132f01ba6e125
Aarish Ramesh <aarish.ramesh@gmail.com>
2020-08-04 19:54:58 +0530

    Refactorted code for ATOI

--- src/arraysstrings/Atoi.java
+++ src/arraysstrings/Atoi.java
@@ -1,19 +1,25 @@
 package arraysstrings;
 
+/**
+ * https://leetcode.com/explore/interview/card/amazon/76/array-and-strings/2962/
+ * 
+ * @author polymath
+ *
+ */
 public class Atoi {
 	
 	public static void main(String[] args) {
-		System.out.println(atoi("-643556183011M11 648G1 903778065 762 75316456373673B5 334 19885 90668 8 98K X277 9846"));
+		System.out.println(atoi("--21"));
 	}
 	
 	public static int atoi(final String A) {
-		int result = 0, tens = 10; boolean intFound = false, signFound = false, minusFound = false;
+		int result = 0, ten = 10; boolean intFound = false, signFound = false, minusFound = false;
 		for (int i = 0; i < A.length(); i++) {
 			char ch = A.charAt(i);
 			if (ch >= 48 && ch <= 57) {
 				if (!intFound)
 					intFound = true;
-				long val = ((long)result * tens) + (ch - 48);
+				long val = ((long)result * ten) + (ch - 48);
 				if (minusFound)
 					val = val * -1;
 				if (val >= Integer.MAX_VALUE)
@@ -21,19 +27,18 @@ public class Atoi {
 				else if (val <= Integer.MIN_VALUE)
 					return Integer.MIN_VALUE;
 				else {
-					result = (result * tens) + (ch - 48);
+					result = (result * ten) + (ch - 48);
 				}
 			} else {
-				if (ch == '-' && !signFound && !intFound) {
-					minusFound = true; 
-					signFound = true;
-				} else if (ch == '+' && !signFound && !intFound) { 
+				if ((ch == '-' || ch == '+') && !signFound && !intFound) {
+					if (ch == '-')
+						minusFound = true; 
 					signFound = true;
 				} else if (ch == ' ') {
-					if (intFound)
+					if (intFound || signFound)
 						break;
 					else
-						return 0;
+						continue;
 				} else {
 					if (!intFound) 
 						return 0;
